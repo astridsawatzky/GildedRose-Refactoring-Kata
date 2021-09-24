df77e3cca8f762f26fdd40d70ea71e8cfa329c3f
astridsawatzky <astrid.sawatzky@gmx.de>
2021-09-24 19:38:02 +0200

    Tets for 2 and 10 days pass

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,7 +2,6 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
@@ -16,10 +15,36 @@ class GildedRoseTest {
         for (int i = 0, itemsLength = items.length; i < itemsLength; i++) {
             Item item = items[i];
 
-            assertEquals(item.quality,itemsAfterOneday[i].quality,i );
-            assertEquals(item.sellIn,itemsAfterOneday[i].sellIn ,i);
-            assertEquals(item.name,itemsAfterOneday[i].name ,"itemno: " + i);
+            assertItemsAreEqual(i, item, itemsAfterOneday[i]);
+          }
+
+        Item[] itemsAfter2days=   TestItemsProvider.itemsAfter2Days();
+        app.updateQuality();
+        for (int i = 0, itemsLength = items.length; i < itemsLength; i++) {
+            Item item = items[i];
+
+
+            assertItemsAreEqual(i, item, itemsAfter2days[i]);
+
         }
+        for (int i =0;i<8;i++){
+            app.updateQuality();
+        }
+        Item[] itemsAfter10days = TestItemsProvider.itemsAfter10Days();
+        for (int i = 0, itemsLength = items.length; i < itemsLength; i++) {
+            Item item = items[i];
+
+
+
+            assertItemsAreEqual(i, item, itemsAfter10days[i]);
+
+        }
+    }
+
+    private void assertItemsAreEqual(int i, Item item, Item item1) {
+        assertEquals(item.quality, item1.quality, "itemno: "+i);
+        assertEquals(item.sellIn, item1.sellIn,"itemno: "+ i);
+        assertEquals(item.name,item1.name ,"itemno: " + i);
 
     }
 
--- Java/src/test/java/com/gildedrose/TestItemsProvider.java
+++ Java/src/test/java/com/gildedrose/TestItemsProvider.java
@@ -22,11 +22,40 @@ public class TestItemsProvider {
             new Item("Elixir of the Mongoose", 4, 6), //
             new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
             new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 14, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21),
             new Item("Backstage passes to a TAFKAL80ETC concert", 9, 50),
             new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50),
             // this conjured item does not work properly yet
             //new Item("Conjured Mana Cake", 3, 6)
         };
     }
-}
+
+    public static Item[] itemsAfter10Days() {
+        return new Item[]{
+            new Item("+5 Dexterity Vest",  0, 10), //
+            new Item("Aged Brie", -8, 18), //
+            new Item("Elixir of the Mongoose", -5, 0), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 35),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50),
+            new Item("Backstage passes to a TAFKAL80ETC concert", -5, 0),
+            // this conjured item does not work properly yet
+            //new Item("Conjured Mana Cake", 3, 6)
+        };
+    }
+
+public static Item[] itemsAfter2Days() {
+    return new Item[]{
+        new Item("+5 Dexterity Vest", 8, 18), //
+        new Item("Aged Brie", 0, 2), //
+        new Item("Elixir of the Mongoose", 3, 5), //
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 13, 22),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 8, 50),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 3, 50),
+        // this conjured item does not work properly yet
+        //new Item("Conjured Mana Cake", 3, 6)
+    };
+}}
