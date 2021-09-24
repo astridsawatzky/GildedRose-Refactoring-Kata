3ba72e67de96f0b435096dd36d89cd31da2f8a34
astridsawatzky <astrid.sawatzky@gmx.de>
2021-09-24 19:45:28 +0200

    make Test more readable

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,42 +9,33 @@ class GildedRoseTest {
     @Test
     void foo() {
         Item[] items = TestItemsProvider.initialItemsTestArray();
-        Item[] itemsAfterOneday = TestItemsProvider.itemsAfterOneDay();
+        Item[] itemsAfterOneDay = TestItemsProvider.itemsAfterOneDay();
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        for (int i = 0, itemsLength = items.length; i < itemsLength; i++) {
-            Item item = items[i];
+        AssertArraysAreEqual(items, itemsAfterOneDay);
 
-            assertItemsAreEqual(i, item, itemsAfterOneday[i]);
-          }
-
-        Item[] itemsAfter2days=   TestItemsProvider.itemsAfter2Days();
+        Item[] itemsAfter2days = TestItemsProvider.itemsAfter2Days();
         app.updateQuality();
-        for (int i = 0, itemsLength = items.length; i < itemsLength; i++) {
-            Item item = items[i];
-
-
-            assertItemsAreEqual(i, item, itemsAfter2days[i]);
-
-        }
-        for (int i =0;i<8;i++){
+        AssertArraysAreEqual(items, itemsAfter2days);
+        for (int i = 0; i < 8; i++) {
             app.updateQuality();
         }
         Item[] itemsAfter10days = TestItemsProvider.itemsAfter10Days();
+        AssertArraysAreEqual(items, itemsAfter10days);
+    }
+
+    private void AssertArraysAreEqual(Item[] items, Item[] otherItems) {
         for (int i = 0, itemsLength = items.length; i < itemsLength; i++) {
             Item item = items[i];
 
-
-
-            assertItemsAreEqual(i, item, itemsAfter10days[i]);
-
+            assertItemsAreEqual(i, item, otherItems[i]);
         }
     }
 
     private void assertItemsAreEqual(int i, Item item, Item item1) {
-        assertEquals(item.quality, item1.quality, "itemno: "+i);
-        assertEquals(item.sellIn, item1.sellIn,"itemno: "+ i);
-        assertEquals(item.name,item1.name ,"itemno: " + i);
+        assertEquals(item.quality, item1.quality, "item No: " + i);
+        assertEquals(item.sellIn, item1.sellIn, "item No: " + i);
+        assertEquals(item.name, item1.name, "item No: " + i);
 
     }
 
