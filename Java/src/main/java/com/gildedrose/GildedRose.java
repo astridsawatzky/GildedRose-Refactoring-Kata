66ecb9e08d84d76f840c70fc6906028b5f2cd045
astridsawatzky <astrid.sawatzky@gmx.de>
2021-09-25 19:06:18 +0200

    funtion over method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -3,90 +3,91 @@ package com.gildedrose;
 import java.util.HashMap;
 import java.util.Map;
 
-interface Assessor{
-           void update(Item item);
-    }
+interface Assessor {
+    void update(Item item);
+}
 
 class GildedRose {
     Item[] items;
 
-    Map<String,Assessor> assessors = new HashMap<>();
+
+    Map<String, Assessor> assessors = new HashMap<>();
 
     public GildedRose(Item[] items) {
         this.items = items;
-        assessors.put("Sulfuras, Hand of Ragnaros",GildedRose::updateSulfurus);
-        assessors.put("Aged Brie",GildedRose::updateAgedBrie);
-        assessors.put("Backstage passes to a TAFKAL80ETC concert",GildedRose::updateBackstagePasses);
+        assessors.put("Sulfuras, Hand of Ragnaros", sulfurus);
+        assessors.put("Aged Brie", agedBrie);
+        assessors.put("Backstage passes to a TAFKAL80ETC concert", backstagePasses);
 
     }
 
     public void updateQuality() {
 
         for (Item item : items) {
-            assessors.getOrDefault(item.name,GildedRose::update).update(item);
+            assessors.getOrDefault(item.name, standard).update(item);
         }
     }
 
 
-    private static void update(Item item) {
-        decreaseSellIn(item);
+
+    Assessor agedBrie = item -> {
+        item.sellIn--;
+        if (item.sellIn >= 0) {
+
+            item.quality = increasedQuality(item, 1);
+
+        } else {
+
+            item.quality = increasedQuality(item, 2);
+
+        }
+    };
+
+
+
+    Assessor backstagePasses = item -> {
+        item.sellIn--;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        } else if (item.sellIn < 5) {
+
+            item.quality = increasedQuality(item, 3);
+
+        } else if (item.sellIn < 10) {
+
+            item.quality = increasedQuality(item, 2);
+
+        } else {
+
+            item.quality = increasedQuality(item, 1);
+
+        }
+    };
+    Assessor standard = item ->
+    {
+        item.sellIn--;
 
         if (item.sellIn >= 0) {
-            decreaseQuality(item, 1);
-        } else {
-            decreaseQuality(item, 2);
-        }
-    }
-    private static void updateSulfurus(Item item) {
 
-    }
-
-    private static void updateBackstagePasses(Item item) {
-        decreaseSellIn(item);
-
-        if (item.sellIn <0) {
-            wipeOutQuality(item);
-        }else
-
-        if (item.sellIn < 5) {
-            increaseQuality(item, 3);
-        }else
-        if (item.sellIn < 10) {
-            increaseQuality(item, 2);
-        } else {
-            increaseQuality(item, 1);
-        }
-    }
-
-    private static void updateAgedBrie(Item item) {
-        decreaseSellIn(item);
-        if (item.sellIn >= 0) {
-            increaseQuality(item, 1);
+            item.quality = decreaseQuality(item, 1);
 
         } else {
-            increaseQuality(item, 2);
+
+            item.quality = decreaseQuality(item, 2);
 
         }
+    };
+
+    Assessor sulfurus = i -> {
+    };
+
+    private int increasedQuality(Item item, int i) {
+        return Math.min(item.quality + i, 50);
+    }
+    private int decreaseQuality(Item item, int i) {
+        return Math.max(item.quality - i, 0);
     }
 
-    private static void decreaseQuality(Item item, int by) {
-
-            item.quality = Math.max(item.quality - by,0);
-
-    }
-
-    private static void increaseQuality(Item item, int by) {
-
-            item.quality = Math.min( item.quality + by,50);
-
-    }
-
-    private static void decreaseSellIn(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private static void wipeOutQuality(Item item) {
-        item.quality = 0;
-    }
 
 }
